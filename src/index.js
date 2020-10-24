82453449f2f0cb18bc2a39db9da6ee3d9918e2d5
Sergei Novikov <1nprint@mail.ru>
2020-10-24 19:52:39 +0400

    сделал блок perfect

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./index.scss";
 import "./pug/UI KIT/Blocks/form elements/Block/range/range.js";
 import "./pug/UI KIT/Blocks/form elements/Block/text field/textfield.js";
+import "./pug/UI KIT/Blocks/form elements/Block/date dropdown/date dropdown.js";
