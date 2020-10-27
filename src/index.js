48521b766fb86d9d1297ecb115b2cd2167494ab5
Sergei Novikov <1nprint@mail.ru>
2020-10-27 11:30:51 +0400

    Сверстал bullet list, для оставшейся версти нужен javaScript. Перехожу к обкчению

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./index.scss";
 import "./pug/UI KIT/Blocks/form elements/Block/range/range.js";
 import "./pug/UI KIT/Blocks/form elements/Block/text field/textfield.js";
 import "./pug/UI KIT/Blocks/form elements/Block/date dropdown/date dropdown.js";
+import "./pug/UI KIT/Blocks/form elements/Block/dropdown/dropdown.js";
