688d03ed2fa4bddbc01bcfcb501835d03876b8f6
Sergei Novikov <1nprint@mail.ru>
2020-11-17 22:25:13 +0400

    переделал скрипт для работы с несколькими элементами

--- src/pug/UI KIT/Blocks/form elements/Block/like button/like button.js
+++ src/pug/UI KIT/Blocks/form elements/Block/like button/like button.js
@@ -1,20 +1,22 @@
 const like = document.querySelectorAll('.likebutton');
 
 for (let i = 0; i < like.length; i++) {
-    const element = array[i];
-    
+    const element = like[i];
+    //-----функция обработчика события----------
+    let likeButton = function  () {
+        this.classList.toggle('likebutton_onclick');
+        let count = this.value;
+        if (this.classList.contains('likebutton_onclick')) {
+            this.value = +count + 1;
+        } else {
+            this.value = +count - 1;
+        }
+    };
+    element.addEventListener('click', likeButton); //Обработчик события элементов массива
 }
 
-//-----функции обработчика события----------
-function likeButton () {
-    this.classList.toggle('likebutton_onclick');
-    let count = like.value;
-    if (this.classList.contains('likebutton_onclick')) {
-        like.value = +count + 1;
-    } else {
-        like.value = +count - 1;
-    }
-}
 
-//-----------Обработчик события-----------
-like.addEventListener('click', likeButton);
+
+
+
+
