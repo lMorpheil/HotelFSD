a418d1817b5e985369f529fd166b6f1ec55f99e3
Сергей Новиков <xain3000@gmail.com>
2020-12-01 14:52:40 +0400

    Немного подредактировал ratebutton

--- src/pug/UI KIT/Blocks/form elements/Block/rate button/rate button.js
+++ src/pug/UI KIT/Blocks/form elements/Block/rate button/rate button.js
@@ -1,12 +1,17 @@
-let star = document.querySelectorAll('.ratebutton__button');
+let rates = document.querySelectorAll('.ratebutton');
 
+for (let i = 0; i < rates.length; i++) {
 
-for (let i = 0; i < star.length; i++) {
-    const element = star[i];
-    let over = function () {
-        for (let j = 0; j <= i; j++) {
-            const elemPlus = star[j];
-            elemPlus.classList.add('ratebutton__button_active');
+    let item = rates[i];
+    let star = item.querySelectorAll('.ratebutton__button');
+    
+    for (let i = 0; i < star.length; i++) {
+
+        const element = star[i];
+        let over = function () {
+            for (let j = 0; j <= i; j++) {
+                const elemPlus = star[j];
+                elemPlus.classList.add('ratebutton__button_active');
         }
         for (let k = star.length - 1; k > i ; k--) {
             const elemMinus = star[k];
@@ -17,3 +22,6 @@ for (let i = 0; i < star.length; i++) {
     };
     element.addEventListener('click', over);
 }
+}
+
+
