4e2fd4813137fbccf55aeee31b2c3598524ad5a5
Сергей Новиков <xain3000@gmail.com>
2020-12-07 23:08:20 +0400

    Настраиваю вторую точку входа

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "./index.scss";
-import "./pug/UI KIT/Blocks/form elements/Block/range/range.js";
-import "./pug/UI KIT/Blocks/form elements/Block/text field/textfield.js";
-import "./pug/UI KIT/Blocks/form elements/Block/date dropdown/date dropdown.js";
-import "./pug/UI KIT/Blocks/form elements/Block/dropdown/dropdown.js";
-import "./pug/UI KIT/Blocks/form elements/Block/like button/like button.js";
-import "./pug/UI KIT/Blocks/form elements/Block/rate button/rate button.js";
-import "./pug/UI KIT/Blocks/form elements/Block/checkbox list/checkbox list.js";
-import "./pug/UI KIT/Blocks/form elements/Block/pagination/pagination.js";
+import "./UI KIT/Blocks/form elements/Block/range/range.js";
+import "./UI KIT/Blocks/form elements/Block/text field/textfield.js";
+import "./UI KIT/Blocks/form elements/Block/date dropdown/date dropdown.js";
+import "./UI KIT/Blocks/form elements/Block/dropdown/dropdown.js";
+import "./UI KIT/Blocks/form elements/Block/like button/like button.js";
+import "./UI KIT/Blocks/form elements/Block/rate button/rate button.js";
+import "./UI KIT/Blocks/form elements/Block/checkbox list/checkbox list.js";
+import "./UI KIT/Blocks/form elements/Block/pagination/pagination.js";
