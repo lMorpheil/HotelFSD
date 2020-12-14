46d26f8b2a672192729d60008ddfe4de5ab82aa7
Сергей Новиков <xain3000@gmail.com>
2020-12-15 01:01:54 +0400

    все ещ мучаюсь с календарём

--- src/UI KIT/Blocks/form elements/Block/date dropdown/date dropdown.js
+++ src/UI KIT/Blocks/form elements/Block/date dropdown/date dropdown.js
@@ -1,40 +1,55 @@
-require("air-datepicker");
-require("air-datepicker/dist/css/datepicker.css");
-require('cleave.js');
+ require("air-datepicker");
+ require("air-datepicker/dist/css/datepicker.css");
+ require('cleave.js');
 
-// let date = document.querySelectorAll('.date__input');
-//     date.value = 
-// console.log($el.date)
+// $(".date").each(function () {
+//   const item = $(this);
+//   const dateFrom = item.find(".date__from");
+//   const dateTo = item.find(".date__to");
 
-$('.datepicker-here').datepicker({
-    navTitles: {
-        days: '<div><div>MM</div> <div>yyyy</div></div>',
-        months: 'yyyy',
-        years: 'yyyy1 - yyyy2'
-    },
-    prevHtml: `<svg>
-                <path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/>
-                </svg>`,
-    nextHtml: `<svg>
-                <path d="M9 0.984375L17.0156 9L9 17.0156L7.59375 15.6094L13.1719 9.98438H0.984375V8.01562H13.1719L7.59375 2.39062L9 0.984375Z" fill="white"/>
-                </svg>`,
-    clearButton: true,
-    todayButton: true,
-    language: {
-        today: 'применить',
-    }
-});
+//   item.find(".datepicker-multi").datepicker({
+//     clearButton: true,
+//     onSelect: (date) => {
+//       const dates = date.split(" - ");
+//       dateFrom.val(dates[0]);
+//       dateTo.val(dates[1]);
+//     }
+//   });
 
-var datepicker = $('.datepicker-here').datepicker().data('datepicker');
-console.log(datepicker.Date)
+//   // Экземпляр класса
+//   const datep = item.find(".datepicker-multi").data("datepicker");
+  
+//   // Экземпляр кода календаря
+//   const datepEl = datep.$datepicker;
+//   // Добавление кнопки
+//   const applyButton = $(
+//     `<span class='datepicker--button'>Применить</span>`
+//   );
+
+//   applyButton.click(() => {
+//     if (datep.selectedDates.length < 2) return;
+//     datep.hide();
+//   });
+
+//   datepEl.find(".datepicker--buttons").append(applyButton);
+//   dateTo.click(() => datep.show());
+//   dateFrom.click(() => datep.show());
+
+//   // Скрытие на клик вне
+//   $(document).mouseup(function (e) {
+//     const dep = $(".datepickers-container");
+//     const wrap = $(".wrap");
+//     if (
+//       !dep.is(e.target) &&
+//       dep.has(e.target).length === 0 &&
+//       !wrap.is(e.target) &&
+//       wrap.has(e.target).length === 0
+//     ) {
+//       datep.hide();
+//     }
+//   });
+// });
 
 
-$('.date__input').toArray().forEach(function(field) {
-    new Cleave(field, {
-        date: true,
-        delimiter: '.',
-        datePattern: ['d', 'm', 'Y']
-    });
- });
 
 
