33addf72306dc3b879b81274ab48cc5968ef4911
Сергей Новиков <xain3000@gmail.com>
2021-03-03 22:04:33 +0400

    Подключил eslinter немного редактировал проект

--- src/js/room-details.js
+++ src/js/room-details.js
@@ -1,3 +1,4 @@
 import '../sass/room-details.scss';
 import '../Blocks/form elements/Block/like button/like button.js'
-import './Blocks/Header&Footer/Blocks/menu/menu.js';
+import '../Blocks/Header&Footer/Blocks/menu/menu.js';
+import '../Blocks/form elements/Block/dropdown/dropdown.js';
