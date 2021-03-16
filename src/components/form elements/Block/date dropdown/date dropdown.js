f36ec305ab2ffb3dcdce995a9c84b1b2b2989b15
Сергей Новиков <xain3000@gmail.com>
2021-03-16 19:18:18 +0400

    делал ревью работы

--- src/components/form elements/Block/date dropdown/date dropdown.js
+++ src/components/form elements/Block/date dropdown/date dropdown.js
@@ -42,3 +42,21 @@ $('.date__from').datepicker({
     $(".date__to").val(fd.split("-")[1]);
   }
 });
+$(document).ready(() => {
+  const inlinePickersCollection = document.querySelectorAll('.js-datepicker-inline');
+
+  inlinePickersCollection.forEach((item) => {
+    $(item).datepicker({
+      range: true,
+      inline: true,
+      clearButton: true,
+      navTitles: {
+        days: 'MM<br>yyyy',
+      },
+    });
+
+    const $calendarEl = $(item).datepicker().data('datepicker').$datepicker;
+    const $buttonsParent = $calendarEl.find('.datepicker--buttons');
+    $buttonsParent.append('<span class="datepicker--button-access">Применить</span>');
+  });
+});
