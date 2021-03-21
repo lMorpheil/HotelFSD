c1e33c72aba738bf571c5b36e12596b5058856cf
Сергей Новиков <xain3000@gmail.com>
2021-03-21 22:13:33 +0400

    Заметил небольшую недоработку в странице room-details

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -1,4 +1,5 @@
 import '../../components/form elements/Block/like button/like button.js'
 import '../../components/form elements/Block/dropdown/dropdown.js';
+import '../../components/form elements/Block/date dropdown/date dropdown.scss';
 import '../../components/Header&Footer/Blocks/menu/menu.js';
 import './room-details.scss';
